c80a15b93d6e07449298d5dd77e4d372a31075d8
trung <tientrung14122012@gmail.com>
2022-09-21 11:11:47 +0700

    profile edit

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,18 +2,20 @@ import styles from "./Profile.module.scss";
 import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAddressCard, faUser } from "@fortawesome/free-regular-svg-icons";
-import { faUserPen } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { faIdCard, faMusic, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useRef, useState } from "react";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
 
 function Profile() {
-  const [styleLine, setStyleLine] = useState({
-    left: 0,
-  });
+  const headerProfile = useRef();
+  const [styleLine, setStyleLine] = useState({});
+  useEffect(() => {
+    const widthLine = headerProfile.current.offsetWidth;
+    setStyleLine({ left: 0, width: widthLine });
+  }, []);
   const handleActiveLine = (e) => {
     if (e.target.tagName === "A") {
       setStyleLine({ left: e.target.offsetLeft, width: e.target.offsetWidth });
@@ -29,7 +31,11 @@ function Profile() {
       <article className={cx("profile")}>
         <div className={cx("profile__Navigate")}>
           <ul className={cx("navigate__List")}>
-            <li onClick={handleActiveLine} className={cx("navigate__item")}>
+            <li
+              ref={headerProfile}
+              onClick={handleActiveLine}
+              className={cx("navigate__item")}
+            >
               <Link to="#" className={cx("navigate__item-link", "autoCenter")}>
                 <FontAwesomeIcon className={cx("iconUser")} icon={faUser} />
                 Profile
@@ -39,9 +45,15 @@ function Profile() {
               <Link to="#" className={cx("navigate__item-link", "autoCenter")}>
                 <FontAwesomeIcon
                   className={cx("iconUserEdit")}
-                  icon={faAddressCard}
+                  icon={faIdCard}
                 />
                 Edit Profile
+              </Link>
+            </li>
+            <li onClick={handleActiveLine} className={cx("navigate__item")}>
+              <Link to="#" className={cx("navigate__item-link", "autoCenter")}>
+                <FontAwesomeIcon className={cx("iconMyMusic")} icon={faMusic} />
+                My Music
               </Link>
             </li>
             <div style={styleLine} className={cx("line")}></div>
