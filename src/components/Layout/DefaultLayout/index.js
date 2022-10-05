9caedd1bba4f2b4437c77ad75dcc2d57fbb01ca3
trung <tientrung14122012@gmail.com>
2022-10-05 10:17:10 +0700

    scroll wrapper

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -3,12 +3,18 @@ import classNames from "classnames/bind";
 
 import Sidebar from "./Sidebar";
 import styles from "./DefaultLayout.module.scss";
+import { useEffect, useRef } from "react";
 
 const cx = classNames.bind(styles);
 
 function DefaultLayout({ children, active }) {
+  const scrollBottom = useRef();
+
+  useEffect(() => {
+    scrollBottom.current.scrollTop = scrollBottom.current.scrollHeight;
+  });
   return (
-    <section className={cx("wrapper")}>
+    <section ref={scrollBottom} className={cx("wrapper")}>
       <Sidebar activeNav={active} />
       <article className={cx("container")}>
         <div className={cx("content")}>{children}</div>
