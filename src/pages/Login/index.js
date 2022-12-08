6cf6db1f298768dcfc5d55060974a609e4601090
trung <tientrung14122012@gmail.com>
2022-12-08 19:48:23 +0700

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,7 +2,7 @@ import styles from "./Login.module.scss";
 import classNames from "classnames/bind";
 import images from "@/assets/images";
 import {
-  FacebookAuthProvider,
+  // FacebookAuthProvider,
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
@@ -13,8 +13,11 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import LoadingLogin from "@/components/Loaddings/LoadingLogin";
 import { setDocument } from "@/firebase/services";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
 
-const fbLogin = new FacebookAuthProvider();
+// const fbLogin = new FacebookAuthProvider();
 const googleLogin = new GoogleAuthProvider();
 const cx = classNames.bind(styles);
 function Login() {
@@ -27,40 +30,40 @@ function Login() {
       }
     }, 1500);
   }, [user, navigate]);
-  const handleLoginFB = () => {
-    signInWithPopup(auth, fbLogin)
-      .then((result) => {
-        // The signed-in user info.
-        const { user, _tokenResponse } = result;
-        console.log("Đăng nhập", { user });
-        if (_tokenResponse.isNewUser) {
-          setDocument("users", {
-            displayName: user.displayName,
-            email: user.email,
-            photoURL: user.photoURL,
-            uid: user.uid,
-            providerId: _tokenResponse.providerId,
-          });
-          // setDocument("userChats", {});
-        }
-        // ...
+  // const handleLoginFB = () => {
+  //   signInWithPopup(auth, fbLogin)
+  //     .then((result) => {
+  //       // The signed-in user info.
+  //       const { user, _tokenResponse } = result;
+  //       console.log("Đăng nhập", { user });
+  //       if (_tokenResponse.isNewUser) {
+  //         setDocument("users", {
+  //           displayName: user.displayName,
+  //           email: user.email,
+  //           photoURL: user.photoURL,
+  //           uid: user.uid,
+  //           providerId: _tokenResponse.providerId,
+  //         });
+  //         // setDocument("userChats", {});
+  //       }
+  //       // ...
 
-        // const credential = FacebookAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-        // console.log("Token", { token });
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = FacebookAuthProvider.credentialFromError(error);
-        console.error({ errorCode, errorMessage, email, credential });
-        // ...
-      });
-  };
+  //       // const credential = FacebookAuthProvider.credentialFromResult(result);
+  //       // const token = credential.accessToken;
+  //       // console.log("Token", { token });
+  //     })
+  //     .catch((error) => {
+  //       // Handle Errors here.
+  //       const errorCode = error.code;
+  //       const errorMessage = error.message;
+  //       // The email of the user's account used.
+  //       const email = error.customData.email;
+  //       // The AuthCredential type that was used.
+  //       const credential = FacebookAuthProvider.credentialFromError(error);
+  //       console.error({ errorCode, errorMessage, email, credential });
+  //       // ...
+  //     });
+  // };
   const handleLoginGoogle = () => {
     signInWithPopup(auth, googleLogin)
       .then((result) => {
@@ -130,22 +133,46 @@ function Login() {
               </div>
             </section>
           </div>
-          <h1 className={cx("Title")}>Hello :))</h1>
+          <h1 className={cx("Title")}>Welcome to Earth</h1>
         </div>
         <div className={cx("body", "autoCenter")}>
-          <div
+          {/* <div
             onClick={handleLoginFB}
             className={cx("signinButton", "autoCenter")}
           >
             <img className={cx("icon")} src={images.facebook} alt="Facebook" />
             <span className={cx("conectTitle")}>Tiếp tục với Facebook</span>
+          </div> */}
+          <div className={cx("signinButton", "autoCenter")}>
+            <FontAwesomeIcon icon={faUser} className={cx("icon")} />
+            <input placeholder="Tài khoản" type="text" />
           </div>
-          <div
+          <div className={cx("signinButton", "autoCenter")}>
+            <FontAwesomeIcon icon={faKey} className={cx("icon")} />
+
+            <input type="text" placeholder="Mật khẩu" />
+          </div>
+          {/* <div
             onClick={handleLoginGoogle}
             className={cx("signinButton", "autoCenter")}
           >
             <img className={cx("icon")} src={images.google} alt="Facebook" />
             <span className={cx("conectTitle")}>Tiếp tục với google</span>
+          </div> */}
+          <div className={cx("account")}>
+            <p>Quên mật khẩu ?</p>
+          </div>
+          <div className={cx("logInApp")}>
+            <div
+              onClick={handleLoginGoogle}
+              className={cx("singInGoogle", "autoCenter")}
+            >
+              {" "}
+              <img className={cx("icon")} src={images.google} alt="Facebook" />
+            </div>
+            <div className={cx("signinButton", "logIn", "autoCenter")}>
+              <p>Đăng nhập</p>
+            </div>
           </div>
         </div>
       </article>
