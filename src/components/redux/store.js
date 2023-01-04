c25ee57ae1227908b6c396343ae5b2692c3ca78e
trung <tientrung14122012@gmail.com>
2023-01-04 22:53:01 +0700

    delete coment

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -7,8 +7,6 @@ import editProfileSlice from "@/pages/Profile/EditProfile/EditProfileSlice";
 import { configureStore } from "@reduxjs/toolkit";
 import SidebarSlide from "../Layout/DefaultLayout/Sidebar/SideBarSlice";
 
-// ý tưởng cho chi tiết tin tức:
-//làm như header nhưng chỉ cần truyền API vào children k cần truyền components
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
